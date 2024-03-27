d6320075f775de05ab3860ec0e6a15e303484769
Josh <jtwilliamson@mnu.edu>
2024-03-27 17:40:01 -0500

    Path Waypoint completion commit

--- main_loop/classes.py
+++ main_loop/classes.py
@@ -6,6 +6,10 @@ TILE_TYPES = 1
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
+PATH = 900
+SPAWN = 901
+BASE = 902
+
 
 
 class Window:
@@ -100,14 +104,11 @@ class GridMap:
         #self.highlightSprite = pygame.sprite.Sprite()
 
     def InitiateGridMap(self, level):
-
-        i = 0
         self.gridMap.clear()
         file_name = "level_data/{level_num}.csv".format(level_num=level)
         with open(file_name, "r") as f:
             csv_reader = csv.reader(f)
             for row in csv_reader:
-
                 for i in range(0, len(row)):
                     if row[i] == "0":
                         row[i] = 0
@@ -125,6 +126,18 @@ class GridMap:
             image = pygame.transform.scale(image, (self.tileSize, self.tileSize))
             self.imageList.append(image)
             print(x)
+        #append path.png
+        image = pygame.image.load(f'assets/tile/path.png').convert_alpha()
+        image = pygame.transform.scale(image, (self.tileSize, self.tileSize))
+        self.imageList.append(image)
+        #append spawn.png
+        image = pygame.image.load(f'assets/tile/spawn.png').convert_alpha()
+        image = pygame.transform.scale(image, (self.tileSize, self.tileSize))
+        self.imageList.append(image)
+        #append base.png
+        image = pygame.image.load(f'assets/tile/base.png').convert_alpha()
+        image = pygame.transform.scale(image, (self.tileSize, self.tileSize))
+        self.imageList.append(image)
 
     def DrawGrid(self, window):
         for row in range(self.rows):
@@ -134,19 +147,29 @@ class GridMap:
             pygame.draw.line(window.display, (0,0,0), (column * self.tileSize, 0),
                              (column * self.tileSize, window.height))
 
+#added 'path', 'spawn', and 'base' handling, need images and function for spawn and base"""
     def DrawTileImage(self):
         for y, row in enumerate(self.gridMap):
             for x, tile in enumerate(row):
-                if tile != 0:
+                if tile == PATH:
+                    image = self.imageList[TILE_TYPES + 1]
+                    pygame.display.get_surface().blit(image, (x * self.tileSize, y * self.tileSize))
+                elif tile == SPAWN:
+                    image = self.imageList[TILE_TYPES + 2]
+                    pygame.display.get_surface().blit(image, (x * self.tileSize, y * self.tileSize))
+                elif tile == BASE:
+                    image = self.imageList[TILE_TYPES + 3]
+                    pygame.display.get_surface().blit(image, (x * self.tileSize, y * self.tileSize))
+                elif tile != 0:
                     image = self.imageList[tile]
                     pygame.display.get_surface().blit(image,
                                                       (x * self.tileSize, y * self.tileSize))
 
     def getTilePosition(self):
         pos = pygame.mouse.get_pos()
-        x = int(pos[0]/self.tileSize)
-        y = int(pos[1]/self.tileSize)
-        return [x, y]
+        column = int(pos[0]/self.tileSize)
+        row = int(pos[1]/self.tileSize)
+        return [row, column]
 
     def getTileValueAtMousePosition(self):
         pos = self.getTilePosition()
@@ -161,14 +184,14 @@ class GridMap:
 
 
         #use in placement events and map drawing for tile locations
-    def updateTile(self, column, row, value):
+
+    def updateTile(self, row, column, value):
         print("value = ", value)
-        print("x, y=", column, row)
+        print("row, column", row, column)
         holder = self.gridMap
         holder[row][column] = value
         self.gridMap = holder
         print("updateTile: ", self.gridMap[row][column])
-
 
     def clickTile(self, left_mouse_button, right_mouse_button):
         pos = self.getTilePosition()
@@ -192,6 +215,131 @@ class GridMap:
         self.updateTile(row, column, 0)
         print("right_click: ", self.gridMap[row][column])
         return
+
+    def getPathStart(self):
+        found_start = False
+
+        self.pathWaypointList.clear()
+
+        while found_start == False:
+            for row in range(self.rows):
+                if found_start == True:
+                    break
+                for column in range(self.columns):
+                    if self.gridMap[row][column] == SPAWN:
+                        self.pathWaypointList.append((row, column))
+                        found_start = True
+                        print("found start")
+                        return
+
+    def writePathWaypointList(self):
+        previous_position = (0, 0)
+        previous_row = previous_position[0]
+        previous_column = previous_position[1]
+
+        index = 0
+        recursion_counter = 0
+        writing_path = True
+
+        self.getPathStart()
+
+        done = False
+        while done == False:
+
+            current_position = (self.pathWaypointList[index])
+            previous_position = self.pathWaypointList[index - 1]
+
+            next_check_list = self.getAdjacentTiles(current_position)
+
+            for pos in next_check_list:
+                if done == True:
+                    break
+                row = pos[0]
+                column = pos[1]
+
+                if pos not in self.pathWaypointList:
+
+                    if self.gridMap[row][column] == 900:
+                        self.pathWaypointList.append((row, column))
+                        index = index + 1
+
+                    elif self.gridMap[row][column] == 902:
+                        self.pathWaypointList.append((row, column))
+                        done = True
+                        print(done)
+                        break
+
+        print("pathWaypointList: ", self.pathWaypointList)
+
+    def getAdjacentTiles(self, current_position):
+        row = current_position[0]
+        column = current_position[1]
+
+        if row > 0:
+            pos1 = (row - 1, column)
+        else:
+            pos1 = (0, column)
+        pos2 = (row + 1, column)
+
+        if column > 0:
+            pos3 = (row, column - 1)
+        else:
+            pos3 = (row, 0)
+        pos4 = (row, column + 1)
+
+        return [pos1, pos2, pos3, pos4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#path_recursion is a WIP method for generating paths"""
+    def path_recursion(self, base, spawn):
+        current_row = 0
+        current_column = 0
+        base_row = base[0]
+        base_column = base[1]
+        spawn_row = spawn[0]
+        spawn_column = spawn[1]
+        self.currentIteration = [spawn_row, spawn_column]
+
+        while self.currentIteration != [base_row, base_column]:
+            current_row = self.currentIteration[0]
+            current_column = self.currentIteration[1]
+
+            for row in enumerate(self.gridMap):
+                for column, tile in enumerate(row):
+                    if current_row == current_row and current_column +- 1 == current_column\
+                            and self.pathWaypointList.index([current_row, current_column]):
+                        self.pathWaypointList
+
+
+
+
 
 
 
