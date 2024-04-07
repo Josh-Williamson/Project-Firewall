ba9216f52fdf61699fb5499aa576500efbf3a22b
Josh <jtwilliamson@mnu.edu>
2024-04-07 10:27:14 -0500

    enemy-class-methods completion, pending PRs before merging main

--- main_loop/main.py
+++ main_loop/main.py
@@ -12,7 +12,8 @@ from enemies import *
 pygame.init()
 
 FPS = pygame.time.Clock()
-FPS.tick(60)
+
+
 
 LEVEL = 0
 
@@ -26,13 +27,13 @@ gridmap.loadImageList()
 gridmap.writePathWaypointList()
 
 loadEnemyImageList(gridmap)
+loadEnemyAttributeList()
 loadProjectileImageList(gridmap)
 
 base = Base()
 
-enemy_1 = Enemy(gridmap)
+enemy_1 = Enemy(gridmap, 1)
 enemy_1.createEnemy((gridmap.getPathStart()), gridmap, base)
-enemy_1.type_ID = 1
 enemy_1.addToGroup()
 
 #basic proof of concept that is drawn on lines 73-74
@@ -41,13 +42,7 @@ projectile_1 = Projectile()
 tower_image1 = pygame.image.load("assets/tower/tower_1.png").convert_alpha()
 tower_1 = Tower((400, 200), tower_image1, 0, projectile_1)
 
-
-
-ENEMY_UPDATE_EVENT = pygame.event.custom_type()
-pygame.time.set_timer(ENEMY_UPDATE_EVENT, 500)
 ENEMY_SPRITE_GROUP.draw(SURFACE)
-
-
 
 keep_game_running = True
 
@@ -68,20 +63,10 @@ while keep_game_running:
         RMB = mouse_pressed[2]
         if (LMB or RMB):
             gridmap.clickTile(LMB, RMB)
-        if event.type == ENEMY_UPDATE_EVENT:
-            ENEMY_SPRITE_GROUP.update(gridmap, base)
 
-
+    ENEMY_SPRITE_GROUP.update(gridmap, base)
 
     ENEMY_SPRITE_GROUP.draw(SURFACE)
-
-    print(ENEMY_SPRITE_GROUP.sprites())
-    print(base.hp)
-
-
-
-    #ENEMY_SPRITE_GROUP.update(gridmap)
-    #ENEMY_SPRITE_GROUP.draw(SURFACE)
 
 
     tower_1.draw(window.display)
@@ -91,7 +76,9 @@ while keep_game_running:
 
     pygame.event.pump()
     pygame.display.flip()
-    FPS.tick_busy_loop(60)
+
+    FPS.tick()
+    print(FPS.get_fps())
 
 else:
     pygame.quit()
